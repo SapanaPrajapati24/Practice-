f8d6b0b2256a97287ff0e3dfe10af8ffeae66133
MAC OS 7 <macos7@MACs-iMac.local>
2022-03-08 13:54:46 +0530

    notes added

---
+++ src/Notes/notes.js
@@ -0,0 +1,8 @@
+//no sense of a lifecycle
+//best used for complex synchronous logic, 
+//Dispatching thunk functions requires that the redux-thunk middleware
+
+//Redux Thunk is middleware that allows you to return functions, rather than just actions, within Redux. 
+//This allows for delayed actions, including working with promises.
+
+//One of the main use cases for this middleware is for handling actions that might not be synchronous,
